94d437385233dae7f50ab4d78452796b0bbe6178
katomasahiro10 <katomasahiro10@gmail.com>
2019-03-26 23:52:03 +0900

    modified: dialy
    完成

--- dialy.sh
+++ dialy.sh
@@ -2,15 +2,17 @@
 
 today=`date '+%Y.%m.%d'`
 thisyear=`echo $today | cut -d '.' -f 1`
+thismonth=`echo $today | cut -d '.' -f 2`
+thisday=`echo $today | cut -d '.' -f 3`
 directory="${HOME}/Dropbox/daily-log/$thisyear"
-dialyfile=$today
+dialyfile="$thismonth.$thisday"
 
-echo $today
-echo $thisyear
-echo $directory
-echo $dialyfile
 if [ ! -d "$directory" ]; then
     mkdir -p "$directory"
 fi
-#if [ ! -e "$dialyfile" ]; then
-    #echo $today
+
+if [ ! -e "$directory/$dialyfile" ]; then
+    echo $today >> "$directory/$dialyfile"
+fi
+
+vim "$directory/$dialyfile"
