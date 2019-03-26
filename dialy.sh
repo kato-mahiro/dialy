89c71a66775ea5b01508cc324887d17c9faa850b
katomasahiro10 <katomasahiro10@gmail.com>
2019-03-26 23:40:39 +0900

    modified: dialy.sh
    ディレクトリの作成まで

--- dialy.sh
+++ dialy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-today=`date '+%Y/%m/%d'`
-thisyear=`echo $today | cut -d '/' -f 1`
-thismonth=`echo $today | cut -d '/' -f 2`
+today=`date '+%Y.%m.%d'`
+thisyear=`echo $today | cut -d '.' -f 1`
 directory="${HOME}/Dropbox/daily-log/$thisyear"
+dialyfile=$today
 
 echo $today
+echo $thisyear
 echo $directory
+echo $dialyfile
 if [ ! -d "$directory" ]; then
     mkdir -p "$directory"
 fi
+#if [ ! -e "$dialyfile" ]; then
+    #echo $today
